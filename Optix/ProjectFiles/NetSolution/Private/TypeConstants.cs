ba5181a90135db8313a419fa99d1ff999ffdb43d
Florish333 <986844987@qq.com>
2024-09-18 15:47:51 +0800

    push all

---
+++ Optix/ProjectFiles/NetSolution/Private/TypeConstants.cs
@@ -0,0 +1,25 @@
+using System;
+using UAManagedCore;
+
+//-------------------------------------------
+// WARNING: AUTO-GENERATED CODE, DO NOT EDIT!
+//-------------------------------------------
+
+namespace NewHMIProject
+{
+    public static class ObjectTypes
+    {
+        private static readonly int namespaceIndex = NamespaceMapProvider.GetNamespaceIndex("NewHMIProject");
+        public static readonly NodeId alarm_panel = new NodeId(namespaceIndex, new Guid("7746246d5b17368c8512cdb55965327c"));
+        public static readonly NodeId Equipment_Phase = new NodeId(namespaceIndex, new Guid("5fac3422ff650a6c0afdbd172251aca8"));
+        public static readonly NodeId Basic_Functions = new NodeId(namespaceIndex, new Guid("7709a7c7f3a16b3bb87631a5afc2c3cb"));
+        public static readonly NodeId MainWindow = new NodeId(namespaceIndex, new Guid("221159a94ea843bd00380306313f9ab0"));
+        public static readonly NodeId Velocity_View = new NodeId(namespaceIndex, new Guid("b440a49ba1798a6cf2a8711a124f0adb"));
+        public static readonly NodeId fa_hui_bu_fen = new NodeId(namespaceIndex, new Guid("66d68a9dc95a8cd347de51a165f711bd"));
+    }
+
+    public static class VariableTypes
+    {
+        private static readonly int namespaceIndex = NamespaceMapProvider.GetNamespaceIndex("NewHMIProject");
+    }
+}
---
+++ Optix/ProjectFiles/NetSolution/Private/UITypeDefinitions.cs
@@ -0,0 +1,36 @@
+using System;
+using UAManagedCore;
+
+//-------------------------------------------
+// WARNING: AUTO-GENERATED CODE, DO NOT EDIT!
+//-------------------------------------------
+
+[MapType(NamespaceUri = "NewHMIProject", Guid = "7746246d5b17368c8512cdb55965327c")]
+public class alarm_panel : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "NewHMIProject", Guid = "5fac3422ff650a6c0afdbd172251aca8")]
+public class Equipment_Phase : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "NewHMIProject", Guid = "7709a7c7f3a16b3bb87631a5afc2c3cb")]
+public class Basic_Functions : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "NewHMIProject", Guid = "221159a94ea843bd00380306313f9ab0")]
+public class MainWindow : FTOptix.UI.Window
+{
+}
+
+[MapType(NamespaceUri = "NewHMIProject", Guid = "b440a49ba1798a6cf2a8711a124f0adb")]
+public class Velocity_View : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "NewHMIProject", Guid = "66d68a9dc95a8cd347de51a165f711bd")]
+public class fa_hui_bu_fen : FTOptix.UI.Panel
+{
+}
